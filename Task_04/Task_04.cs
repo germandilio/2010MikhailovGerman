db6f5dc6dcd50080918108fcc5442fd0df3674ea
germandilio <glmikhaylov@edu.hse.ru>
2020-09-05 21:52:42 +0300

    First commit

---
+++ Task_04/Task_04.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            double U = Convert.ToDouble(str1);
+
+            string str2 = Console.ReadLine();
+            double R = Convert.ToDouble(str2);
+
+            double I = U * R;
+            double P = Math.Pow(U,2) / R;
+
+            Console.WriteLine("Сила тока = ");
+            Console.WriteLine(I);
+            Console.WriteLine("Мощность = ");
+            Console.WriteLine(P);
+
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Task_05/Task_05.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = Console.ReadLine();
+            double leg1 = Convert.ToDouble(str1);
+
+            string str2 = Console.ReadLine();
+            double leg2 = Convert.ToDouble(str2);
+            
+            double hypotenuse = Math.Sqrt(Math.Pow(leg1, 2) + Math.Pow(leg2, 2));
+            Console.WriteLine("Гипотенуза = ");
+            Console.WriteLine(hypotenuse);
+        }
+    }
+}
