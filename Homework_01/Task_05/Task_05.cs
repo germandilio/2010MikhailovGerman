8b4f06e53b405963c1d236e697fdb90a63550788
germandilio <70810123+germandilio@users.noreply.github.com>
2020-09-09 16:25:39 +0300

    Second commit

--- Homework_01/Task_05/Task_05.cs
+++ Homework_01/Task_05/Task_05.cs
@@ -9,7 +9,7 @@ namespace Task_05
             double leg1 = Convert.ToDouble(Console.ReadLine());
 
             double leg2;
-            bool a = double.TryParse(Console.ReadLine(), out leg2));
+            bool a = double.TryParse(Console.ReadLine(), out leg2);
             
             double hypotenuse = Math.Sqrt(Math.Pow(leg1, 2) + Math.Pow(leg2, 2));
             Console.WriteLine("Гипотенуза = " + hypotenuse);
