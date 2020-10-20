efa4f93b52e6cb26e4a07651448fb5a671fc92d8
germandilio <glmikhaylov@edu.hse.ru>
2020-10-20 17:32:36 +0300

    Restoring

---
+++ File_Manager/File_Manager/Main.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Collections;
+
+/*  
+    Файл содержит метод Main, в котором определется ОС и в зависимости от нее запускает графический интерфейс для
+    Mac OS и Linux, или (коммандный) интерфейс терминала для Windows.
+
+    line 20 - 40 метод  Main().
+    line 43 - 195 все кроссплатформенные методы.
+
+*/
+
+namespace Peergrade
+{
+    partial class FileManager
+    {
+        // Шапка, которая будет подсказывать пользователю что сейчас надо сделать.
+        private static string headToPrint;
+
+        /// <summary>
+        /// Метод является переклюяателем между двумя разными реализациями приложения(для Windows, Linux и Mac).
+        /// </summary>
+        /// <param name="args"></param>
+        private static void Main(string[] args)
+        {
+            // Получение информации об операционной системе.
+            string systemIO = Environment.OSVersion.ToString();
+
+            if (systemIO.ToLower().Contains("unix") || systemIO.ToLower().Contains("linux"))
+            {
+                // ОС - Mac OS или Linux.
+                RunForUnix();
+            }
+            else 
+            {
+                // ОС - Microsoft Windows.
+                RunForWindows();
+            }
+        }
+
+
+        /// <summary>
+        /// Метод для получения информации о файле по указанному пути в директории.
+        /// </summary>
+        /// <param name="dir">Путь до директории</param>
+        static void GetInformationAboutFile(string pathtofile)
+        {
+            try
+            {
+                Console.Clear();
+                // Проверка на существование файла.
+                if (File.Exists(pathtofile))
+                {
+                    Console.WriteLine($"Файл {Path.GetFileName(pathtofile)} :");
+                    // Время создания.
+                    Console.WriteLine($"Время создания: {File.GetCreationTime(pathtofile)}");
+                    // Время последнего обращения.
+                    Console.WriteLine($"Время последнего обращения: {File.GetLastAccessTime(pathtofile)}");
+                    // Время последнего изменения.
+                    Console.WriteLine($"Время последнего изменения: {File.GetLastWriteTime(pathtofile)}");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Пожалуйста, выберите только поддерживаемыq файл ");
+                Console.WriteLine("Ошибка: " + ex.Message);
+                // Красивый выход в начало через ESC.
+                return;
+            }
+
+        }
+
+
+        /// <summary>
+        /// Метод вывода текстового файла в консоль, в выбранной кодировке.
+        /// </summary>
+        /// <param name="pathToFile">Путь к файлу, содержимое котрого надо вывести.</param>
+        /// <param name="encoding">Кодировка.</param>
+        private static void ConsoleOutputFileInSelectedEncoding(string pathToFile, Encoding encoding)
+        {
+            // Обработка исклюяений.
+            try
+            {
+                string[] output = File.ReadAllLines(pathToFile, encoding);
+                // Вывод на экран.
+                Console.Clear();
+                Console.WriteLine("Текст из файла: ");
+                for (int i = 0; i < output.Length; i++)
+                {
+                    Console.WriteLine(output[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Ошибка: " + ex.Message);
+                // Красивый выход в начало через ESC.
+                return;
+            }
+
+        }
+
+
+        /// <summary>
+        /// Метод возвращает выор пользователя(пункт подменю или кодировка).
+        /// </summary>
+        /// <param name="Items">Пункты подменю.</param>
+        /// <returns>Выбор пользователя.</returns>
+        private static int GetUserItem(string[] Items)
+        {
+            // Метод подсвечивает выбор пользователеля возвращает номер выбранного пункта подменю (При нажатии Tab).
+
+            Console.CursorVisible = false;
+            // Чтобы не было мигающего курсора.
+            int choice = 0;
+            while (true)
+            {
+                PrintDirectories(Items, choice);
+
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        choice--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        choice++;
+                        break;
+                    case ConsoleKey.Enter:
+                        return choice;
+                }
+                // Зацикливаем выбор.
+                choice = (choice + Items.Length) % Items.Length;
+            }
+        }
+
+
+        /// <summary>
+        /// Метод является переключателем кодировок, в зависимости от выбора пользователя.
+        /// </summary>
+        /// <returns>Выбранную кодировку&</returns>
+        private static Encoding ChooseEncoding()
+        {
+            headToPrint = "ВЫБЕРИТЕ КОДИРОВКУ: (для подтверждения выбора нажмите Enter)";
+
+            string[] Encodings = { "UTF-8", "UTF-32", "Unicode", "ASCII", };
+            int item = GetUserItem(Encodings);
+            // Свитчер выбора кодировок.
+            switch (item)
+            {
+                case 0:
+                    return Encoding.UTF8;
+                case 1:
+                    return Encoding.UTF32;
+                case 2:
+                    return Encoding.Unicode;
+                case 3:
+                    return Encoding.ASCII;
+                default:
+                    return Encoding.UTF8;
+            }
+        }
+
+
+        /// <summary>
+        /// Метод печатает передоваемое меню, подсвечивая красным цветом текущий выбор пользователя.
+        /// </summary>
+        /// <param name="Directories">Массив отображаемых названий.</param>
+        /// <param name="choice">Выобор пользователя.</param>
+        private static void PrintDirectories(string[] Directories, int choice)
+        {
+            Console.Clear();
+            // Печать шапки, которая подсказывает что делать.
+            Console.WriteLine(headToPrint);
+            Console.WriteLine();
+            // Текущуюю директорию.
+
+            for (int i = 0; i < Directories.Length; i++)
+            {
+
+
+                if (i == choice)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Directories[i]);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine(Directories[i]);
+                }
+            }
+        }
+
+
+
+    }
+}
---
+++ File_Manager/File_Manager/Windows.cs
@@ -0,0 +1,390 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+/*  
+    Реализация консольного интерфейса для Windows.
+*/
+
+namespace Peergrade
+{
+    partial class FileManager
+    {
+
+        /// <summary>
+        /// Метод создания текстового файла.
+        /// </summary>
+        /// <param name="pathToFile">Путь директории, в которой будет расположен файл.</param>
+        /// <param name="encoding">Кодировка.</param>
+        private static void CreateFileInSelectedEncodingWindows(string pathToFile, Encoding encoding)
+        {
+            Console.Clear();
+            string pathToChosenFile = pathToFile + Path.VolumeSeparatorChar + "NewTextFile.txt";
+
+            Console.WriteLine("Введите текст, который хотите записать в файл, затем нажмите Enter ");
+            Console.CursorVisible = true;
+            // Обработка исключений.
+            try
+            {
+                string line = Console.ReadLine();
+                File.WriteAllText(pathToChosenFile, line, encoding);
+            }
+            catch (Exception ex)
+            {
+                Console.Clear();
+                Console.WriteLine("Ошибка создания файла: " + ex.Message);
+                // Красивый выход в начало через ESC.
+                return;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Готово!!! Ваш файл расположен в : ");
+            Console.WriteLine(pathToChosenFile);
+
+        }
+
+        /// <summary>
+        /// Метод печатает в консоль правила использования файлового менеджера.
+        /// </summary>
+        private static void HelpMenuWindows()
+        {
+            // Вызов подсказок в самом начале, как пользоваться программой.
+            Console.WriteLine();
+            Console.WriteLine("     Добро пожаловать в Файловый менеджер !!!                                                         ");
+            Console.WriteLine(" -----------------------------------------------------------------------------------------------------");
+            Console.WriteLine(" Файловый менеджер распознает следующие команды:                                                      ");
+            Console.WriteLine(" -----------------------------------------------------------------------------------------------------");
+            Console.WriteLine(@" cd - означает переместиться в директорию по указанному пути (change directory).                     ");
+            Console.WriteLine(@" vf - посмотреть файлы и папки в данной директори (view files).                                      ");
+            Console.WriteLine(@" mv - Переместить файл (move directory).                                                             ");
+            Console.WriteLine(@" rmdir - Удалить пустую папку (remove directory).                                                    ");
+            Console.WriteLine(@" rm - удалить файл.                                                                                  ");
+            Console.WriteLine(@" rm -r  - удаление папки и всех вложенных файлов.                                                    ");
+            Console.WriteLine(@" info - получить информацию о файле.                                                                 ");
+            Console.WriteLine(@" print - вывести в консоль текстовый файл.                                                           ");
+            Console.WriteLine(@" concat - сконкотенировать файлы в один, и вывести в консоль.                                        ");
+            Console.WriteLine(@" exit - выйти из терминала.                                                                          ");
+            Console.WriteLine(" -----------------------------------------------------------------------------------------------------");
+            Console.WriteLine("  ВНИМАНИЕ!!! Следует сначала вводить желаемую команду, нажать Enter, а затем вводить путь.           ");
+            Console.WriteLine(" -----------------------------------------------------------------------------------------------------");
+            Console.WriteLine(" Нажмите Enter, чтобы продолжить ...                                                                  ");
+            Console.WriteLine(" Или любую другую клавишу, чтобы выйти.                                                               ");
+        }
+
+       
+        
+        /// <summary>
+        /// Метод запускает файловый менеджер для Windows.
+        /// </summary>
+        private static void RunForWindows()
+        {
+            // Печать меню.
+            HelpMenuWindows();
+            // Для продолжения нужно ввести Enter.
+            if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+            {
+                do
+                {
+                    Console.Clear();
+                    try
+                    {
+                        // Запускаемм терминал.
+                        Commands();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(" Ошибка: " + ex.Message);
+                        Environment.Exit(0);
+                    }
+                    // После совершения операции запрашивать продолжение.
+
+                    Console.WriteLine("------------------------------------------------------------------");
+                    Console.WriteLine("Для выхода нажмите Esc, или любую другую клавишу чтобы продолжить ");
+                } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Тело файлового менеджера (интерфейс терминала).
+        /// </summary>
+        private static void Commands()
+        { 
+
+            string command = String.Empty;
+            do
+            {
+                    // Получаем директорию, в которой сейчас находимся.
+                    string dir = Directory.GetCurrentDirectory();
+                    // Печатаем текущий путь.
+                    Console.WriteLine(dir + ": ");
+                    // Считываем команду пользователя.
+                    command = Console.ReadLine();
+                    command.TrimEnd(' ');
+                // Свитчер по командам.
+                switch (command)
+                {
+                    case "cd":
+                        {
+                            Console.WriteLine(" Введите путь к файлу, который хотите переместиться: ");
+                            try
+                            {
+                                // Переместиться в директорию.
+                                string path = Console.ReadLine();
+                                Directory.SetCurrentDirectory(path);
+
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Ошибка: " + ex.Message);
+                            }
+                            break;
+                        }
+                    case "vf":
+                        {
+                            try
+                            {
+                                // Посмотреть все файлы в директории.
+                                foreach (string file in Directory.EnumerateFileSystemEntries(dir))
+                                {
+                                    Console.WriteLine(file.Split(Path.VolumeSeparatorChar)[file.Split(Path.VolumeSeparatorChar).Length - 1]);
+                                }
+                                Console.WriteLine();
+
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine(" Ошибка: " + ex.Message);
+                            }
+                            break;
+                        }
+                    case "mv":
+                        {
+                            try
+                            {
+                                // Переместить файл.
+                                Console.WriteLine(" Введите путь к файлу, который хотите переместить: ");
+                                string path1 = Console.ReadLine();
+                                string path2 = Console.ReadLine();
+
+                                Directory.Move(path1, path2);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine(" Ошибка: " + ex.Message);
+                            }
+                            break;
+                        }
+                    case "rmdir":
+                        {
+                            try
+                            {
+                                Console.WriteLine(" Введите путь к файлу, который хотите  удалить: ");
+                                string path = Console.ReadLine();
+
+                                if (Directory.EnumerateFileSystemEntries(path).Count() > 0)
+                                {
+                                    Console.WriteLine(" Папка не пустая, для удаления данной папки воспользуйтесь командой  \"rm -r\" ");
+                                }
+                                else
+                                {
+                                    // Удалить директорию.
+                                    Directory.Delete(path);
+                                }
+
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Ошибка: " + ex.Message);
+                            }
+                            break;
+                        }
+                    case "rm":
+                        {
+                            try
+                            {
+                                Console.WriteLine(" Введите путь к файлу, который хотите  удалить: ");
+                                string path = Console.ReadLine();
+                                // Удалить файл.
+                                Directory.Delete(path);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Ошибка: " + ex.Message);
+                            }
+
+                            break;
+                        }
+                    case "rm -r":
+                        {
+                            try
+                            {
+                                Console.WriteLine(" Введите путь к файлу, который хотите  удалить: ");
+                                string path = Console.ReadLine();
+                                // Удалить папку и все файлы в ней.
+                                Directory.Delete(path, true);
+
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Ошибка: " + ex.Message);
+                            }
+                            break;
+                        }
+
+                    case "info":
+                        {
+                            try
+                            {
+                                Console.WriteLine(" Введите путь к файлу, информацию о котором хотите узнать: ");
+                                string path = Console.ReadLine();
+                                // Получить информацию о файле.
+                                GetInformationAboutFile(path);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Ошибка: " + ex.Message);
+                            }
+                            break;
+                        }
+
+                    case "exit":
+                        {
+                            command = "exit";
+                            break;
+                        }
+                    // Печать в консоль.
+                    case "print":
+                        {
+                            try
+                            {
+                                Console.WriteLine(" Введите путь к файлу, который хотите вывести в консоль: ");
+                                string path = Console.ReadLine();
+                                Encoding enc = ChooseEncoding();
+                                // Вызов печати.
+                                ConsoleOutputFileInSelectedEncoding(path, enc);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Ошибка: " + ex.Message);
+                            }
+                            Console.WriteLine();
+                            break;
+
+                        }
+                    case "create":
+                        {
+                            try
+                            {
+                                Console.WriteLine(" Введите путь к создаваемому файлу: ");
+                                string path = Console.ReadLine();
+
+                                Encoding enc = ChooseEncoding();
+                                // Вызов создания файла.
+                                CreateFileInSelectedEncodingWindows(path, enc);
+
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Ошибка: " + ex.Message);
+                            }
+                            Console.WriteLine();
+                            break;
+                        }
+                    case "concat":
+                        {
+                            try
+                            {
+                                Console.WriteLine(" Введите количество конкатенируемых файлов! ");
+                                string path = Console.ReadLine();
+                                int numbersOfFiles;
+                                // Парсим число.
+                                if (!int.TryParse(path, out numbersOfFiles) || numbersOfFiles < 1)
+                                {
+                                    Console.WriteLine(" Введено некорректное число файлов! ");
+                                    break;
+                                }
+                                else
+                                {
+                                    Console.WriteLine(" Введите путь к файлу: ");
+                                    // Считываем путь.
+                                    string pathToFile = Console.ReadLine();
+                                    // Прочитали первый файл.
+                                    string[] output = File.ReadAllLines(pathToFile);
+                                    // Создаем файл по пути.
+
+                                    string pathToChosenFile = Directory.GetParent(pathToFile).ToString() + Path.VolumeSeparatorChar + "NewConcatTextFile.txt";
+                                    File.AppendAllLines(pathToChosenFile, output, Encoding.UTF8);
+                                    // Обрабатываем остальные файлы.
+                                    for (int i = 2; i <= numbersOfFiles; i++)
+                                    {
+                                        Console.WriteLine(" Введите путь к файлу: ");
+                                        string pathtoFile = Console.ReadLine();
+                                        // Записываем файл.
+                                        output = File.ReadAllLines(pathToFile);
+                                        File.AppendAllLines(pathToChosenFile, output, Encoding.UTF8);
+                                    }
+                                    Console.WriteLine("----------------------------");
+                                    Console.WriteLine("Готово!!! Ваш файл лежит в: ");
+                                    Console.WriteLine(pathToChosenFile);
+
+                                    // Вызываем печать файла в консоль.
+                                    try
+                                    {
+                                        string[] textToPrint = File.ReadAllLines(pathToFile, Encoding.UTF8);
+                                        // Вывод на экран.
+                                        Console.WriteLine("----------------");
+                                        Console.WriteLine("Текст из файла: ");
+                                        for (int i = 0; i < output.Length; i++)
+                                        {
+                                            Console.WriteLine(output[i]);
+                                        }
+                                    }
+                                    catch (Exception ex)
+                                    {
+                                        Console.Clear();
+                                        Console.WriteLine("Ошибка конкотенации файлов: " + ex.Message);
+                                        // Красивый выход в начало через ESC.
+                                        return;
+                                    }
+
+
+                                }
+
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Ошибка: " + ex.Message);
+                            }
+                            Console.WriteLine();
+                            break;
+                        }
+
+                    default:
+                        {
+                            Console.WriteLine(" Неизвестная команда! ");
+                            Console.WriteLine();
+                            break;
+                        }
+
+                }
+
+            } while (command != "exit");
+
+        }
+    }
+}
+
